776f08db14642b9f51422457f865aed91dee3979
GrischK <grischka.gorski@gmail.com>
2022-04-05 16:12:20 +0200

    fin du jeu

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-const Fighter = require("./src/Fighter.js")
+const Fighter = require("./src/Fighter.js");
 // First Labour : Heracles vs Nemean Lion
-const Heracles = new Fighter("Heracles", 20, 16, MAX_LIFE)
-const Nemean = new Fighter("Nemean", 11, 13, MAX_LIFE)
+
+const heracles = new Fighter("🛡️ Heracles ⚔️", 20, 6);
+const nemean = new Fighter("🦁 Nemean 🦁", 11, 13);
 // use your Figher class here
-console.log(Heracles)
-console.log(Nemean)
+
+console.log(`${heracles.name} is ready with ${heracles.life} 💙`);
+console.log(`${nemean.name} is ready with ${nemean.life} 💙`);
+
+heracles.fight(nemean);
+
+// Boucle pour le combat
+let round = 1;
+
+while (heracles.life > 0 && nemean.life > 0) {
+  console.log(`round ${(round)}`);
+
+  heracles.fight(nemean);
+  console.log(
+    `${heracles.name} is attacking ${nemean.name} who has now ${nemean.life} life points`
+  );
+
+  nemean.fight(heracles);
+  console.log(
+    `${nemean.name} is attacking ${heracles.name} who has now ${heracles.life} life points`
+  );
+
+  round++
+  }
+
+  if (heracles.life <= 0) {
+      console.log(`🏆 ${nemean.name} wins (💙${nemean.life})`)
+      console.log(`💀 ${heracles.name} is dead`)
+  }
+
+  if (nemean.life <= 0) {
+      console.log(`🏆 ${heracles.name} wins (💙${heracles.life})`)
+      console.log(`💀 ${nemean.name} is dead`)
+  }
+
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,12 +1,27 @@
 MAX_LIFE = 100;
 
 class Fighter {
-    constructor(name, strenght, dexterity, life) {
-        this.name = name;
-        this.strenght = strenght;
-        this.dexterity = dexterity;
-        this.life = life;
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+  /**
+   * Attaque l'ennemi passé en paramètre
+   */
+  fight(enemy) {
+    let damage = Math.floor(Math.random() * this.strength + 1);
+    damage -= enemy.dexterity;
+    // console.log(damage)
+    // Si aucun dommage n'est causé, l'ennemie évite le coup
+    if (damage > 0) {
+      enemy.life -= damage;
+    //   console.log(
+    //     `${this.name} is attacking ${enemy.name} and give him ${damage} damage points. ${enemy.name} has now ${enemy.life} life points`
+    //   );
     }
+  }
 }
 
-module.exports = Fighter/* Fighter class definition */
+module.exports = Fighter; /* Fighter class definition */
