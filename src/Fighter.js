8893a4a057e2d509e809c9cc729cdb6251b6ea35
GrischK <grischka.gorski@gmail.com>
2022-04-05 10:01:07 +0200

    creating class fighter

---
+++ src/Fighter.js
@@ -0,0 +1,10 @@
+MAX_LIFE = 100;
+
+class Fighter {
+    constructor(name, strenght, dexterity, life) {
+        this.name = name;
+        this.strenght = strenght;
+        this.dexterity = dexterity;
+        this.life = life;
+    }
+}/* Fighter class definition */
